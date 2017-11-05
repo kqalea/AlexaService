aada57a47fb0932fca207765144a431c981745c5
Kqalea <kqalea@gmail.com>
2017-11-05 23:04:21 +0800

    Initial commit

---
+++ app/src/main/java/com/ecs/alexaservice/AlexaService.java
@@ -0,0 +1,297 @@
+package com.ecs.alexaservice;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.media.AudioManager;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.IBinder;
+import android.os.Looper;
+import android.os.Message;
+import android.os.Process;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.widget.Toast;
+
+import com.willblaschko.android.alexa.AlexaManager;
+import com.willblaschko.android.alexa.audioplayer.AlexaAudioPlayer;
+import com.willblaschko.android.alexa.callbacks.AsyncCallback;
+import com.willblaschko.android.alexa.interfaces.AvsItem;
+import com.willblaschko.android.alexa.interfaces.AvsResponse;
+import com.willblaschko.android.alexa.interfaces.audioplayer.AvsPlayContentItem;
+import com.willblaschko.android.alexa.interfaces.audioplayer.AvsPlayRemoteItem;
+import com.willblaschko.android.alexa.interfaces.playbackcontrol.AvsMediaNextCommandItem;
+import com.willblaschko.android.alexa.interfaces.playbackcontrol.AvsMediaPauseCommandItem;
+import com.willblaschko.android.alexa.interfaces.playbackcontrol.AvsMediaPlayCommandItem;
+import com.willblaschko.android.alexa.interfaces.playbackcontrol.AvsMediaPreviousCommandItem;
+import com.willblaschko.android.alexa.interfaces.playbackcontrol.AvsReplaceAllItem;
+import com.willblaschko.android.alexa.interfaces.playbackcontrol.AvsReplaceEnqueuedItem;
+import com.willblaschko.android.alexa.interfaces.playbackcontrol.AvsStopItem;
+import com.willblaschko.android.alexa.interfaces.speaker.AvsAdjustVolumeItem;
+import com.willblaschko.android.alexa.interfaces.speaker.AvsSetMuteItem;
+import com.willblaschko.android.alexa.interfaces.speaker.AvsSetVolumeItem;
+import com.willblaschko.android.alexa.interfaces.speechrecognizer.AvsExpectSpeechItem;
+import com.willblaschko.android.alexa.interfaces.speechsynthesizer.AvsSpeakItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by kqalea on 11/5/17.
+ */
+
+public class AlexaService extends Service {
+
+    public static final String PRODUCT_ID = "DemoServiceECS201711";
+    public static final String TAG = "AlexaService";
+
+    private Looper mServiceLooper;
+    private ServiceHandler mServiceHandler;
+
+    private AlexaManager alexaManager;
+    private AlexaAudioPlayer audioPlayer;
+    private List<AvsItem> avsQueue = new ArrayList<>();
+
+    private void initAlexaAndroid(){
+        alexaManager = AlexaManager.getInstance(this, PRODUCT_ID);
+        audioPlayer = AlexaAudioPlayer.getInstance(this);
+        audioPlayer.addCallback(alexaAudioPlayerCallback);
+    }
+
+    private AlexaAudioPlayer.Callback alexaAudioPlayerCallback = new AlexaAudioPlayer.Callback() {
+        @Override
+        public void playerPrepared(AvsItem pendingItem) {
+
+        }
+
+        @Override
+        public void playerProgress(AvsItem currentItem, long offsetInMilliseconds, float percent) {
+
+        }
+
+        @Override
+        public void itemComplete(AvsItem completedItem) {
+            avsQueue.remove(completedItem);
+            checkQueue();
+        }
+
+        @Override
+        public boolean playerError(AvsItem item, int what, int extra) {
+            return false;
+        }
+
+        @Override
+        public void dataError(AvsItem item, Exception e) {
+
+        }
+    };
+
+    private AsyncCallback<AvsResponse, Exception> requestCallback = new AsyncCallback<AvsResponse, Exception>() {
+        @Override
+        public void start() {
+
+        }
+
+        @Override
+        public void success(AvsResponse result) {
+            Log.i(TAG, "Voice Success");
+            handleResponse(result);
+        }
+
+        @Override
+        public void failure(Exception error) {
+
+        }
+
+        @Override
+        public void complete() {
+
+        }
+    };
+
+    private void handleResponse(AvsResponse response){
+        if(response != null){
+            //if we have a clear queue item in the list, we need to clear the current queue before proceeding
+            //iterate backwards to avoid changing our array positions and getting all the nasty errors that come
+            //from doing that
+            for(int i = response.size() - 1; i >= 0; i--){
+                if(response.get(i) instanceof AvsReplaceAllItem || response.get(i) instanceof AvsReplaceEnqueuedItem){
+                    //clear our queue
+                    avsQueue.clear();
+                    //remove item
+                    response.remove(i);
+                }
+            }
+            avsQueue.addAll(response);
+        }
+        checkQueue();
+    }
+
+    /**
+     * Check our current queue of items, and if we have more to parse (once we've reached a play or listen callback) then proceed to the
+     * next item in our list.
+     *
+     * We're handling the AvsReplaceAllItem in handleResponse() because it needs to clear everything currently in the queue, before
+     * the new items are added to the list, it should have no function here.
+     */
+    private void checkQueue() {
+
+        //if we're out of things, hang up the phone and move on
+        if (avsQueue.size() == 0) {
+            return;
+        }
+
+        AvsItem current = avsQueue.get(0);
+
+        if (current instanceof AvsPlayRemoteItem) {
+            //play a URL
+            if (!audioPlayer.isPlaying()) {
+                audioPlayer.playItem((AvsPlayRemoteItem) current);
+            }
+        } else if (current instanceof AvsPlayContentItem) {
+            //play a URL
+            if (!audioPlayer.isPlaying()) {
+                audioPlayer.playItem((AvsPlayContentItem) current);
+            }
+        } else if (current instanceof AvsSpeakItem) {
+            //play a sound file
+            if (!audioPlayer.isPlaying()) {
+                audioPlayer.playItem((AvsSpeakItem) current);
+            }
+        } else if (current instanceof AvsStopItem) {
+            //stop our play
+            audioPlayer.stop();
+            avsQueue.remove(current);
+        } else if (current instanceof AvsReplaceAllItem) {
+            audioPlayer.stop();
+            avsQueue.remove(current);
+        } else if (current instanceof AvsReplaceEnqueuedItem) {
+            avsQueue.remove(current);
+        } else if (current instanceof AvsExpectSpeechItem) {
+            //listen for user input
+            audioPlayer.stop();
+            startListening();
+        } else if (current instanceof AvsSetVolumeItem) {
+            setVolume(((AvsSetVolumeItem) current).getVolume());
+            avsQueue.remove(current);
+        } else if(current instanceof AvsAdjustVolumeItem){
+            adjustVolume(((AvsAdjustVolumeItem) current).getAdjustment());
+            avsQueue.remove(current);
+        } else if(current instanceof AvsSetMuteItem){
+            setMute(((AvsSetMuteItem) current).isMute());
+            avsQueue.remove(current);
+        }else if(current instanceof AvsMediaPlayCommandItem){
+            //fake a hardware "play" press
+            sendMediaButton(this, KeyEvent.KEYCODE_MEDIA_PLAY);
+        }else if(current instanceof AvsMediaPauseCommandItem){
+            //fake a hardware "pause" press
+            sendMediaButton(this, KeyEvent.KEYCODE_MEDIA_PAUSE);
+        }else if(current instanceof AvsMediaNextCommandItem){
+            //fake a hardware "next" press
+            sendMediaButton(this, KeyEvent.KEYCODE_MEDIA_NEXT);
+        }else if(current instanceof AvsMediaPreviousCommandItem){
+            //fake a hardware "previous" press
+            sendMediaButton(this, KeyEvent.KEYCODE_MEDIA_PREVIOUS);
+        }
+
+    }
+
+    //our call to start listening when we get a AvsExpectSpeechItem
+    protected void startListening(){
+
+    };
+
+    //adjust our device volume
+    private void adjustVolume(long adjust){
+        setVolume(adjust, true);
+    }
+
+    //set our device volume
+    private void setVolume(long volume){
+        setVolume(volume, false);
+    }
+
+    //set our device volume, handles both adjust and set volume to avoid repeating code
+    private void setVolume(final long volume, final boolean adjust){
+        AudioManager am = (AudioManager) getSystemService(AUDIO_SERVICE);
+        final int max = am.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        long vol= am.getStreamVolume(AudioManager.STREAM_MUSIC);
+        if(adjust){
+            vol += volume * max / 100;
+        }else{
+            vol = volume * max / 100;
+        }
+        am.setStreamVolume(AudioManager.STREAM_MUSIC, (int) vol, AudioManager.FLAG_VIBRATE);
+        //confirm volume change
+        alexaManager.sendVolumeChangedEvent(volume, vol == 0, requestCallback);
+    }
+
+    //set device to mute
+    private void setMute(final boolean isMute){
+        AudioManager am = (AudioManager) getSystemService(AUDIO_SERVICE);
+        am.setStreamMute(AudioManager.STREAM_MUSIC, isMute);
+        //confirm device mute
+        alexaManager.sendMutedEvent(isMute, requestCallback);
+    }
+
+    private static void sendMediaButton(Context context, int keyCode) {
+        KeyEvent keyEvent = new KeyEvent(KeyEvent.ACTION_DOWN, keyCode);
+        Intent intent = new Intent(Intent.ACTION_MEDIA_BUTTON);
+        intent.putExtra(Intent.EXTRA_KEY_EVENT, keyEvent);
+        context.sendOrderedBroadcast(intent, null);
+
+        keyEvent = new KeyEvent(KeyEvent.ACTION_UP, keyCode);
+        intent = new Intent(Intent.ACTION_MEDIA_BUTTON);
+        intent.putExtra(Intent.EXTRA_KEY_EVENT, keyEvent);
+        context.sendOrderedBroadcast(intent, null);
+    }
+
+    private final class ServiceHandler extends Handler {
+        public ServiceHandler(Looper looper) {
+            super(looper);
+        }
+        @Override
+        public void handleMessage(Message msg) {
+            stopSelf(msg.arg1);
+        }
+    }
+    @Override
+    public void onCreate() {
+        // Start up the thread running the service.  Note that we create a
+        // separate thread because the service normally runs in the process's
+        // main thread, which we don't want to block.  We also make it
+        // background priority so CPU-intensive work will not disrupt our UI.
+        initAlexaAndroid();
+        HandlerThread thread = new HandlerThread("ServiceStartArguments",
+                Process.THREAD_PRIORITY_BACKGROUND);
+        thread.start();
+
+        // Get the HandlerThread's Looper and use it for our Handler
+        mServiceLooper = thread.getLooper();
+        mServiceHandler = new ServiceHandler(mServiceLooper);
+    }
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        Toast.makeText(this, "service starting", Toast.LENGTH_SHORT).show();
+        Message msg = mServiceHandler.obtainMessage();
+        msg.arg1 = startId;
+        mServiceHandler.sendMessage(msg);
+
+        // If we get killed, after returning from here, restart
+        return START_STICKY;
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // We don't provide binding, so return null
+        return null;
+    }
+
+    @Override
+    public void onDestroy() {
+        Toast.makeText(this, "service done", Toast.LENGTH_SHORT).show();
+    }
+
+}
